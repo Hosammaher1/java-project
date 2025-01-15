b28b58a9e2dcaf455eada9482eb7f6822a72c820
Hosammaher1 <hosam.maher48@gmail.com>
2025-01-15 14:11:18 +0200

    Add files via upload

---
+++ project/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ATMSystem atmSystem = new ATMSystem();
+        atmSystem.start();
+
+    }
+}
+//7152468
